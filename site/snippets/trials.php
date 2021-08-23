d6e9be482ab56ef2c8df7e2b4da71822f7d37da6
Kyle Benson <kyle.benson@intercom.io>
2021-08-23 12:37:27 -0700

    Swapping out bloated mailchimp validator

--- site/snippets/trials.php
+++ site/snippets/trials.php
@@ -11,41 +11,34 @@
   https://getkirby.com/docs/guide/templates/snippets
 */
 ?>
-<h2><a name="trial-fonts">Font Trials</a></h2>
-<p>Free trial downloads are available by subscribing to our marketing newsletter. After submitting the form below, you’ll receive a confirmation email—accept that email and you'll receive a final email with a link to a zip folder chock full of trial fonts for the entire catalog. Our trials have a limited character set, no kerning, and no opentype features. Happy trials!
+<h2 class="margin-s"><a name="trial-fonts">Font Trials</a></h2>
+<p class="margin-l">A zip folder chock full of trial fonts for the entire catalog is available after subscribing to our marketing newsletter. Our trials have a limited character set, no kerning, and no opentype features. Happy trials!
 </p>
-<form action="//verycoolstudio.us4.list-manage.com/subscribe/post?u=edc2737100be661c0fc9e2c5e&amp;id=1e6678ac80" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="trial-fonts-fields validate" target="_blank" novalidate="novalidate">
-  <div>
-    <div class="validate-required">
-      <label class="margin-s" for="mce-EMAIL"><h5>Email</h5></label>
-      <input class="margin-s" type="email" value="" name="EMAIL" class="input-text required email" id="mce-EMAIL" aria-required="true">
+<form action="//verycoolstudio.us4.list-manage.com/subscribe/post?u=edc2737100be661c0fc9e2c5e&amp;id=1e6678ac80" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank">
+  <div class="autogrid margin-s" style="--gutter: var(--margin-s); align-items: end;">
+    <div>
+      <label for="mce-FNAME"><h5 class="margin-s">First Name</h5></label>
+      <input type="text" name="FNAME" id="mce-FNAME">
     </div>
-    <div class="autogrid margin-s" style="--gutter: .75vw; align-items: end;">
-      <div class="form-row-first">
-        <label class="margin-s" for="mce-FNAME"><h5>First</h5></label>
-        <input class="margin-s" type="text" value="" name="FNAME" class="input-text " id="mce-FNAME">
-      </div>
-      <div class="form-row-last">
-        <label class="margin-s" for="mce-LNAME"><h5>Last</h5></label>
-        <input class="margin-s" type="text" value="" name="LNAME" class="input-text " id="mce-LNAME">
-      </div>
-      <div>
-        <input class="margin-s button" type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe">
-      </div>
-    </div>
-    <div class="terms terms-and-conditions flex" style="align-items: center;">
-      <input class="margin-s input-checkbox" type="checkbox" name="terms" id="terms" style="margin-right: 10px;">
-      <label class="checkbox" for="terms"><h6 class="margin-s">I accept the trail <a href="/trial-terms" class="link" target="_blank">terms &amp; conditions</a></h6></label>
-      <input class="margin-s" type="hidden" name="terms-field" value="1">
-    </div>
-    <div id="mce-responses">
-      <div class="response padding-sm bg-white color-grey rounded-corners" id="mce-error-response" style="display:none;"></div>
-      <div class="response padding-sm bg-white color-grey rounded-corners" id="mce-success-response" style="display:none;"></div>
-    </div>
-    <div style="position: absolute; left: -5000px;" aria-hidden="true">
-      <input class="margin-s" type="text" name="b_edc2737100be661c0fc9e2c5e_1e6678ac80" tabindex="-1" value="">
+    <div>
+      <label for="mce-LNAME"><h5 class="margin-s">Last Name</h5></label>
+      <input type="text" name="LNAME" id="mce-LNAME">
     </div>
   </div>
+	<div>
+		<label for="mce-EMAIL"><h5 class="margin-s">Email Address</h5></label>
+		<input class="margin-s" type="email" name="EMAIL" id="mce-EMAIL" title="The domain portion of the email address is invalid (the portion after the @)." pattern="^([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22))*\x40([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d))*(\.\w{2,})+$" required>
+	</div>
+  <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_f2d244c0df42a0431bd08ddea_aeaa9dd034" tabindex="-1" value=""></div>
+  <div class="terms terms-and-conditions flex margin-m" style="align-items: center;">
+    <input class="margin-s input-checkbox" type="checkbox" name="terms" id="terms" style="margin-right: 10px;">
+    <label class="checkbox" for="terms"><h6 class="margin-s">I accept the trail <a href="/trial-terms" class="link" target="_blank">terms &amp; conditions</a></h6></label>
+    <input class="margin-s" type="hidden" name="terms-field" value="1">
+  </div>
+  <div class="margin-s">
+    <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button">
+  </div>
+  <div class="mc-status"></div>
 </form>
-<script type="text/javascript" src="//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js"></script>
-<script type="text/javascript">(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
+
+<?php echo js('assets/js/mailchimp-min.js?v='.time().'') ?>
